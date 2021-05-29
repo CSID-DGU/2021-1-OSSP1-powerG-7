78d07fa92ad8416cd6a3d9aff0c816a3281a0ff4
kangjeonghun <hunida6650@gmail.com>
2021-05-29 17:46:23 +0900

    라우터 추가

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,105 @@
+"use strict";
+
+const User = require("../../models/User");
+
+const output = {
+    home: (req, res) => {
+        if(req.session.is_logined == true){
+            res.render("home/",{
+                is_logined : true
+            });
+        }else{
+            res.render("home/",{
+                is_logined : false
+            });
+        }
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+    register: (req, res) => {
+        res.render("home/register");
+    },
+    FAQ: (req, res) => {
+        if(req.session.is_logined == true){
+            res.render("home/FAQ",{
+                is_logined : true
+            });
+        }else{
+            res.render("home/FAQ",{
+                is_logined : false
+            });
+        }
+    },
+    logout: (req, res) => {
+        req.session.destroy(function(err) {
+            res.redirect('/');
+        });
+    },
+    shop1: (req, res) => {
+        if(req.session.is_logined == true){
+            res.render("home/shop/1",{
+                is_logined : true
+            });
+        }else{
+            res.render("home/shop/1",{
+                is_logined : false
+            });
+        }
+    },
+    shop2: (req, res) => {
+        if(req.session.is_logined == true){
+            res.render("home/shop/2",{
+                is_logined : true
+            });
+        }else{
+            res.render("home/shop/2",{
+                is_logined : false
+            });
+        }
+    },
+    shop3: (req, res) => {
+        if(req.session.is_logined == true){
+            res.render("home/shop/3",{
+                is_logined : true
+            });
+        }else{
+            res.render("home/shop/3",{
+                is_logined : false
+            });
+        }
+    },
+    shop4: (req, res) => {
+        if(req.session.is_logined == true){
+            res.render("home/shop/4",{
+                is_logined : true
+            });
+        }else{
+            res.render("home/shop/4",{
+                is_logined : false
+            });
+        }
+    }
+};
+
+const process = {
+    login: async (req, res) => {
+      const user = new User(req.body);
+      const response = await user.login();
+
+      if (response.success == true) req.session.is_logined = true;
+      else req.session.is_logined = false;
+
+      return res.json(response);
+    },
+    register: async (req, res) => {
+      const user = new User(req.body);
+      const response = await user.register();
+      return res.json(response);
+    }
+};
+
+module.exports = {
+    output,
+    process,
+};
---
+++ app/src/routes/home/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require("express");
+const app = require("../../../app");
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+router.get("/", ctrl.output.home);
+router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
+router.get("/FAQ", ctrl.output.FAQ);
+router.get("/logout", ctrl.output.logout);
+router.get("/shop/1", ctrl.output.shop1);
+router.get("/shop/2", ctrl.output.shop2);
+router.get("/shop/3", ctrl.output.shop3);
+router.get("/shop/4", ctrl.output.shop4);
+
+router.post("/login", ctrl.process.login);
+router.post("/register", ctrl.process.register);
+
+module.exports = router;
