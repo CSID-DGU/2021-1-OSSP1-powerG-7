04d317de8287d7d28249eefbab844feade6e4b8a
Hyerin So <78003321+hrinee@users.noreply.github.com>
2021-06-04 16:32:50 +0900

    Update PGToken.sol

--- test_ethereum/PGToken.sol
+++ test_ethereum/PGToken.sol
@@ -1,32 +1,29 @@
 pragma solidity ^0.4.16;
 
 contract PGToken {
-   string public name="powerG";
-   string public symbol="PG";
+   string public name="PGToken";
+   string public symbol="PGT";
    uint8 public decimals=0;
+   uint256 public totalSupply;
 
    mapping (address => uint256) public balanceOf;
 
    event Transfer(address _from, address _to, uint _value);
-    
-    function getBalanceOf(address _to) public view returns (uint256) {
-        return balanceOf[_to];
-    }
-    function setBalanceOf(address _to, uint256 _value) public {
-        balanceOf[_to]=_value;
+
+   function PGToken (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
-    function transfer(address _to, uint256 _value) {
-        setBalanceOf(msg.sender, 1000);
-        setBalanceOf(_to, 1000);
-
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         if (balanceOf[msg.sender] < _value) revert();
         if (balanceOf[_to] + _value < balanceOf[_to]) revert();
 
-        setBalanceOf(msg.sender, balanceOf[msg.sender]-_value); // Subtract from the sender
-                        
-        setBalanceOf(_to, balanceOf[_to]+_value); // Add the same to the recipient
+        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
 
         Transfer(msg.sender,_to,_value);
+        
+        return true;
     }
 }
