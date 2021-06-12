b6809317bdf7765c108bc0c9c325501f692b23df
Hyerin So <78003321+hrinee@users.noreply.github.com>
2021-06-12 12:14:46 +0900

    db config 수정
    테이블 생성

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -1,13 +1,32 @@
 //db 수정해야
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: '',
+let db =  mysql.createConnection({
+    host: 'localhost',
     user: 'root',
     password: '0000',
-    database: '',
+    database: 'powerg', //데이터베이스 미리 생성해놔야함
 });
 
-db.connect();
+db.connect(function (err) {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    let query1 = 'use powerg;'
+    let query2 = 'create table if not exists users(id varchar(10), name varchar(10), pw varchar(10));';
+
+    db.query(query1, function (err, results, fields) {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+    db.query(query2, function (err, results, fields) {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+
+});
 
 module.exports = db;
