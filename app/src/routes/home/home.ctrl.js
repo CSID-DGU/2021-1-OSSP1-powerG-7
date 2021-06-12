2c6c18bfca742e3268ed6e03c1d12651fd20d5b7
kangjeonghun <hunida6650@gmail.com>
2021-06-12 17:51:35 +0900

    개발자 화면 분리

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,16 +1,27 @@
 "use strict";
 
+const e = require("express");
 const User = require("../../models/User");
 
 const output = {
     home: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/",{
-                is_logined : true
-            });
-        }else{
-            res.render("home/",{
-                is_logined : false
+        if (req.session.is_logined == true) {
+            if (req.session.dev == true) {
+                res.render("home/devhome", {
+                    dev: true,
+                    is_logined: true
+                })
+            }
+            else {
+                res.render("home/", {
+                    dev: false,
+                    is_logined: true
+                })
+            }
+        }
+        else {
+            res.render("home/", {
+                is_logined: false
             });
         }
     },
@@ -21,95 +32,95 @@ const output = {
         res.render("home/register");
     },
     FAQ: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/FAQ",{
-                is_logined : true
+        if (req.session.is_logined == true) {
+            res.render("home/FAQ", {
+                is_logined: true
             });
-        }else{
-            res.render("home/FAQ",{
-                is_logined : false
+        } else {
+            res.render("home/FAQ", {
+                is_logined: false
             });
         }
     },
     logout: (req, res) => {
-        req.session.destroy(function(err) {
+        req.session.destroy(function (err) {
             res.redirect('/');
         });
     },
     overview: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/overview",{
-                is_logined : true
+        if (req.session.is_logined == true) {
+            res.render("home/overview", {
+                is_logined: true
             });
-        }else{
-            res.render("home/overview",{
-                is_logined : false
-            });
-        }
-    },
-	dev: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/dev",{
-                is_logined : true
-            });
-        }else{
-            res.render("home/dev",{
-                is_logined : false
+        } else {
+            res.render("home/overview", {
+                is_logined: false
             });
         }
     },
-	mypage: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/mypage",{
-                is_logined : true
+    dev: (req, res) => {
+        if (req.session.is_logined == true) {
+            res.render("home/dev", {
+                is_logined: true
             });
-        }else{
-            res.render("home/mypage",{
-                is_logined : false
+        } else {
+            res.render("home/dev", {
+                is_logined: false
+            });
+        }
+    },
+    mypage: (req, res) => {
+        if (req.session.is_logined == true) {
+            res.render("home/mypage", {
+                is_logined: true
+            });
+        } else {
+            res.render("home/mypage", {
+                is_logined: false
             });
         }
     },
     shop1: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/shop/1",{
-                is_logined : true
+        if (req.session.is_logined == true) {
+            res.render("home/shop/1", {
+                is_logined: true
             });
-        }else{
-            res.render("home/shop/1",{
-                is_logined : false
+        } else {
+            res.render("home/shop/1", {
+                is_logined: false
             });
         }
     },
     shop2: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/shop/2",{
-                is_logined : true
+        if (req.session.is_logined == true) {
+            res.render("home/shop/2", {
+                is_logined: true
             });
-        }else{
-            res.render("home/shop/2",{
-                is_logined : false
+        } else {
+            res.render("home/shop/2", {
+                is_logined: false
             });
         }
     },
     shop3: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/shop/3",{
-                is_logined : true
+        if (req.session.is_logined == true) {
+            res.render("home/shop/3", {
+                is_logined: true
             });
-        }else{
-            res.render("home/shop/3",{
-                is_logined : false
+        } else {
+            res.render("home/shop/3", {
+                is_logined: false
             });
         }
     },
     shop4: (req, res) => {
-        if(req.session.is_logined == true){
-            res.render("home/shop/4",{
-                is_logined : true
+        if (req.session.is_logined == true) {
+            res.render("home/shop/4", {
+                is_logined: true
             });
-        }else{
-            res.render("home/shop/4",{
-                is_logined : false
+        } else {
+            res.render("home/shop/4", {
+                is_logined: false
             });
         }
     }
@@ -117,18 +128,29 @@ const output = {
 
 const process = {
     login: async (req, res) => {
-      const user = new User(req.body);
-      const response = await user.login();
+        const user = new User(req.body);
+        const response = await user.login();
 
-      if (response.success == true) req.session.is_logined = true;
-      else req.session.is_logined = false;
+        if (response.success == true) {
+            req.session.is_logined = true;
+            if (user.body.id == 'developer') {
+                req.session.dev = true;
+            }
+            else {
+                req.session.dev = false;
+            }
+        }
+        else {
+            req.session.is_logined = false;
+            req.session.dev = false;
+        }
 
-      return res.json(response);
+        return res.json(response);
     },
     register: async (req, res) => {
-      const user = new User(req.body);
-      const response = await user.register();
-      return res.json(response);
+        const user = new User(req.body);
+        const response = await user.register();
+        return res.json(response);
     }
 };
 
